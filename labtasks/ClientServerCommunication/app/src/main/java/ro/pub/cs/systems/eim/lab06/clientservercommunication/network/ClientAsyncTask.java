13858669d53625c1585641898bca368e18143870
iulia-ni <iulia.nicula@stud.acs.upb.ro>
2018-05-15 13:28:19 +0300

    L6

--- labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
+++ labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
@@ -4,7 +4,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
 import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Constants;
+import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Utilities;
 
 public class ClientAsyncTask extends AsyncTask<String, String, Void> {
 
@@ -16,14 +21,24 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
 
     @Override
     protected Void doInBackground(String... params) {
+        Socket socket = null;
         try {
 
             // TODO exercise 6b
             // - get the connection parameters (serverAddress and serverPort from parameters - on positions 0 and 1)
+            String serverAddress = params[0];
+            int serverPort = Integer.parseInt(params[1]);
             // - open a socket to the server
+            socket = new Socket(serverAddress, serverPort);
+
             // - get the BufferedReader in order to read from the socket (use Utilities.getReader())
+            BufferedReader bufferedReader = Utilities.getReader(socket);
             // - while the line that has read is not null (EOF was not sent), append the content to serverMessageTextView
             // by publishing the progress - with the publishProgress(...) method - to the UI thread
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                publishProgress(line);
+            }
             // - close the socket to the server
 
         } catch (Exception exception) {
@@ -31,6 +46,15 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
             if (Constants.DEBUG) {
                 exception.printStackTrace();
             }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.v(Constants.TAG, "Connection closed");
         }
         return null;
     }
@@ -39,12 +63,14 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
     protected void onPreExecute() {
         // TODO exercise 6b
         // - reset the content of the serverMessageTextView
+        serverMessageTextView.setText("");
     }
 
     @Override
     protected void onProgressUpdate(String... progress) {
         // TODO exercise 6b
         // - append the content to serverMessageTextView
+        serverMessageTextView.append(progress[0] + '\n');
     }
 
     @Override
--- labtasks/FTPServerWelcomeMessage/app/src/main/java/ro/pub/cs/systems/eim/lab06/ftpserverwelcomemessage/network/FTPServerCommunicationAsyncTask.java
+++ labtasks/FTPServerWelcomeMessage/app/src/main/java/ro/pub/cs/systems/eim/lab06/ftpserverwelcomemessage/network/FTPServerCommunicationAsyncTask.java
@@ -4,9 +4,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.Socket;
 
 import ro.pub.cs.systems.eim.lab06.ftpserverwelcomemessage.general.Constants;
+import ro.pub.cs.systems.eim.lab06.ftpserverwelcomemessage.general.Utilities;
 
 public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, Void> {
 
@@ -22,8 +25,23 @@ public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, V
         try {
             // TODO exercise 4
             // open socket with FTPServerAddress.getText().toString() (taken from param[0]) and port (Constants.FTP_PORT = 21)
+            String hostname = params[0];
+            int port = Constants.FTP_PORT;
+            socket = new Socket(hostname, port);
+
             // get the BufferedReader attached to the socket (call to the Utilities.getReader() method)
+            BufferedReader bufferedReader = Utilities.getReader(socket);
             // should the line start with Constants.FTP_MULTILINE_STARTCODE = "220-", the welcome message is processed
+            String line = bufferedReader.readLine();
+            Log.v(Constants.TAG, "Received line: " + line);
+            if (line != null && line.startsWith(Constants.FTP_MULTILINE_START_CODE)) {
+                while((line = bufferedReader.readLine()) != null) {
+                    if (!Constants.FTP_MULTILINE_END_CODE1.equals(line) && !line.startsWith(Constants.FTP_MULTILINE_END_CODE2)) {
+                        Log.v(Constants.TAG, "Received line: " + line);
+                        publishProgress(line);
+                    }
+                }
+            }
             // read lines from server while
             // - the value is different from Constants.FTP_MULTILINE_END_CODE1 = "220"
             // - the value does not start with Constants.FTP_MULTILINE_END_CODE2 = "220 "
@@ -33,6 +51,14 @@ public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, V
             Log.d(Constants.TAG, exception.getMessage());
             if (Constants.DEBUG) {
                 exception.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return null;
@@ -47,6 +73,7 @@ public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, V
     protected void onProgressUpdate(String... progres) {
         // TODO exercise 4
         // append the progress[0] to the welcomeMessageTextView text view
+        welcomeMessageTextView.append(progres[0] + "\n");
     }
 
     @Override
